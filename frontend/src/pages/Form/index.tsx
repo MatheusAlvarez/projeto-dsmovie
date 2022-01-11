59a104369850c8ef9edaed8efe238c92b0d3c0d7
Matheus Maia Alvarez <mthalvarez0000@hotmail.com>
2022-01-11 11:34:28 -0300

    Navigation Buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './styles.css';
 
 function Form() {
@@ -34,7 +36,10 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+
             </div >
         </div >
     );
