d5c9a1c3e289872fb7b420e391fe33d1b51025b2
Matheus Maia Alvarez <mthalvarez0000@hotmail.com>
2022-01-11 10:04:41 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <h1>Página Principal</h1>
+    <Navbar />
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg'; 
+
+import './styles.css';
+
+function Navbar(){
+    
+    return(
+        <header>
+        <nav className="container">
+          <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+              <div className="dsmovie-contact-container">
+                <GithubIcon />
+                <p className="dsmovie-contact-link">/devsuperior</p>
+              </div>
+            </a>
+          </div>
+        </nav>
+      </header>
+    );
+}
+
+export default Navbar;
